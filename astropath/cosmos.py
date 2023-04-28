9e4f2d43e15730b4c012958579228729a5153dde
profxj <xaiver@ucolick.org>
2023-04-28 12:54:15 -0700

    wip

--- astropath/cosmos.py
+++ astropath/cosmos.py
@@ -1,7 +1,60 @@
 """ Definitions and methods related to COSMOS """
 
+import tqdm
+import numpy as np
+
+import pandas
+
+from astropy import units
+from astropy.coordinates import SkyCoord 
+
 def cosmos_defs():
     cdefs = dict(plate_scale=0.05,
                  filter='ACS_i',
                  )
     return cdefs
+
+def load_galaxies(cosmos_file:str='cosmos_acs_iphot_200709.feather'):
+    # Load
+    df = pandas.read_feather(cosmos_file)
+
+    # Cut
+    df = df[df['mu_class'] == 1]
+    df = df[~np.isnan(df['kron_radius'])]
+
+    pix_to_arcsec_kron = 0.03*units.arcsec
+    df['half_light'] = df['kron_radius']*pix_to_arcsec_kron #arcsec
+
+    # Return
+    return df
+
+
+def gen_frb_galaxy_pairs(cosmos_df:pandas.DataFrame,
+    a_sigma:units.Quantity=1*units.arcsec,
+    b_sigma:units.Quantity=1*units.arcsec,
+    pa_mode:str='fixed',
+    pa_value:float=0.):
+# pix_to_arcsec = 0.05*units.arcsec
+
+    frbs = []
+    frb_with_gal = []
+
+    for i, row in tqdm.tqdm(cosmos_df.iterrows()):
+        coord = SkyCoord(ra=row['ra']*units.degree, dec=row['dec']*u.degree)
+
+        # 
+        loc_uncertainity = np.random.normal(scale=loc_sigma.value)*units.arcsec
+        loc_pa = float(np.random.uniform(size=1, low=0., high=360.))
+        coord_with_loc_uncertainity = coord.directional_offset_by(loc_pa*units.deg, loc_uncertainity)
+
+    #     theta_max = pix_to_arcsec*row['flux_radius']
+        theta_max = 2*row['half_light']
+        galaxy_offset = np.random.uniform(low=0, high=theta_max)*units.arcsec
+        gal_pa = float(np.random.uniform(size=1, low=0., high=360.))
+        frb_coord = coord_with_loc_uncertainity.directional_offset_by(gal_pa*units.deg, galaxy_offset)
+
+        row['frb_ra'] = frb_coord.ra.value
+        row['frb_dec'] = frb_coord.dec.value
+        frbs.append([row['name'], row['frb_ra'], row['frb_dec'], loc_sigma.value])
+        
+        frb_with_gal.append(row)
--- calculations/CHIME/GBO/py/monte_carlo.py
+++ calculations/CHIME/GBO/py/monte_carlo.py
@@ -8,14 +8,26 @@ import pandas
 from matplotlib import pyplot as plt
 import seaborn as sns
 
+from astropy import units
+
+from astropath import cosmos
+from astropy.coordinates import SkyCoord, match_coordinates_sky
+
 from IPython import embed
 
-def generate_frbs(chime_mr_tbl:str='CHIME_mr_5Jyms_150.parquet',
-                  m_r_lim:float=23.3, nsample=100000,
-                  debug:bool=False):
+def generate_frbs(chime_mr_tbl:str='CHIME_mr_5Jyms_150.parquet', 
+                  m_r_min:float=15.,
+                  m_r_max:float=26.,
+                  nsample=100000,
+                  debug:bool=False,
+                  plots:bool=False):
 
     # Load up m_r distribution
     chime_mr = pandas.read_parquet(chime_mr_tbl)
+    m_r_cuts = (chime_mr.m_r < m_r_max) & (
+            chime_mr.m_r > m_r_min)
+    chime_mr = chime_mr[m_r_cuts]
+   
     if debug:
         chime_mr=chime_mr[0:100000]
     n_chime = len(chime_mr)
@@ -31,7 +43,7 @@ def generate_frbs(chime_mr_tbl:str='CHIME_mr_5Jyms_150.parquet',
     rand = np.random.uniform(size=nsample)
     rand_mr = f_cdf(rand)
 
-    if debug:
+    if plots:
         plt.clf()
         ax = plt.gca()
 
@@ -50,9 +62,54 @@ def generate_frbs(chime_mr_tbl:str='CHIME_mr_5Jyms_150.parquet',
         plt.show()
 
     # Load COSMOS
+    cosmos_df = cosmos.load_galaxies()
+    ra_deg = cosmos_df.ra.values * units.deg
+    dec_deg = cosmos_df.dec.values * units.deg
+    ra_min, ra_max = ra_deg.min(), ra_deg.max()
+    dec_min, dec_max = dec_deg.min(), dec_deg.max()
+
+    cut_ra = (ra_deg > (ra_min + 1*units.arcmin)) & (
+        ra_deg < (ra_max - 1*units.arcmin))
+    cut_dec = (dec_deg > (dec_min + 1*units.arcmin)) & (
+        dec_deg < (dec_max - 1*units.arcmin))
 
     # Cut me
+    cuts = cut_dec & cut_ra
+    cosmos_cut = cosmos_df[cuts]
+
+    # Choose the galaxies
+    fake_coords = SkyCoord(ra=np.ones(nsample),
+                           dec=rand_mr, unit='deg')
+    fake_cosmos = SkyCoord(ra=np.ones(len(cosmos_cut)),
+                           dec=cosmos_cut.mag_best.values,
+                           unit='deg')
+
+    cosmos_flag = np.ones_like(cosmos_cut, dtype=bool)
+    cosmos_idx = cosmos_cut.index.values.copy()
+    frb_idx = -1*np.ones_like(cosmos_cut, dtype=int)
+
+    while(np.any(frb_idx < 0)):
+
+        # Sub me
+        sub_fake_coords = fake_coords[frb_idx < 0]
+        sub_fake_cosmos = fake_cosmos[cosmos_flag]
+        sub_cosmos_idx = cosmos_idx[cosmos_flag]
+
+        idx, d2d, _ = match_coordinates_sky(
+            sub_fake_coords, sub_fake_cosmos)
+
+        imx = np.argmax(d2d)
+        print(f'Max: {sub_fake_coords[imx]}')
+        print(f'sep = {d2d[imx]}')
+
+        # Specify them
+        uni, uni_idx = np.unique(idx, return_index=True)
+        embed(header='59 of mc')
+
+        cosmos_flag[sub_cosmos_idx[uni]] = False
+
+
 
 # Command line execution
 if __name__ == '__main__':
-    generate_frbs(debug=True)
+    generate_frbs(debug=True, plots=False)
