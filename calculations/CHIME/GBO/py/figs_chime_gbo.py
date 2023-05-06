3d6f68b43efde68ce7b05057be33266a8aff814f
profxj <xavier@ucolick.org>
2023-05-06 06:06:07 -0700

    first fig

--- calculations/CHIME/GBO/py/figs_chime_gbo.py
+++ calculations/CHIME/GBO/py/figs_chime_gbo.py
@@ -7,17 +7,44 @@ import pandas
 import seaborn as sns
 from matplotlib import pyplot as plt
 
+from frb.figures import utils as ffutils
 
-def simple_stats(): 
-    pass
+import analysis
+
+
+def fig_simple_stats(path_file:str, frb_file:str,
+                     outfile:str): 
+    # parse
+    frbs = analysis.parse_PATH(path_file, frb_file)
+
+    ax = sns.scatterplot(frbs, x='mag', y='P_Ox', s=3)
+
+    # High confidednce
+    mag_lim = 20.
+    PATH_lim = 0.9
+    high = frbs.P_Ox > PATH_lim
+    bright = frbs.mag < mag_lim
+
+    high_conf = frbs[high & bright]
+
+    ax.text(0.05, 0.05, f'N(P_Ox>{PATH_lim}; m<{mag_lim})={len(high_conf)}/{len(frbs)}', 
+            fontsize=14, transform=ax.transAxes)
+
+
+    # Finish
+    ffutils.set_fontsize(ax, 16.)
+
+    pad = 0.2
+    plt.tight_layout(pad=0.2,h_pad=pad,w_pad=pad)
+
+    plt.savefig(outfile, dpi=300)#, bbox_inches='tight')
+    plt.close()
+    print(f"Wrote: {outfile}")
 
 # Command line execution
 if __name__ == '__main__':
 
     # Generate FRBs
-    #generate_frbs('frb_monte_carlo.csv',
-    #    debug=True, plots=False, nsample=10000)
-
-    # Monte Carlo
-    #run_mc('first_try.csv', debug=True)
-    run_mc('frb_monte_carlo_15x3.csv', 'PATH_15x3.csv')#, debug=True)
+    fig_simple_stats('PATH_15x3.csv', 
+                     'frb_monte_carlo_15x3.csv',
+                     'simple_stats_15x3.png')
