aacd16d3f60c3488271c17cd4c1d501da1ec302b
J. Xavier Prochaska <xavier@ucolick.org>
2026-06-09 08:02:56 -0700

    ok

--- calculations/step_size/test_accuracy.py
+++ calculations/step_size/test_accuracy.py
@@ -48,6 +48,60 @@ def init_localization(a, b):
 
     return Path
 
+def single_case(outfile:str, a, b, galaxy_size, 
+                gal_offset:float=3., step_sizes=None,
+                gal_pa:float=0.):
+
+    box_hwidth = max(5 * a, gal_offset + 6 * galaxy_size) # arcsec
+
+    # FRB
+    path = init_localization(a, b)
+
+    # Galaxy
+    gal_coord = path.localiz['center_coord'].directional_offset_by(
+        gal_pa*units.deg, gal_offset*units.arcsec)
+
+    # Step sizes
+    if step_sizes is None:
+        step_sizes = np.array([0.02, 0.05, 0.1, 0.2, 0.5, 1., 2.]) # fast
+
+    raw_pxO = np.zeros((1, len(step_sizes)))
+    cor_pxO = np.zeros((1, len(step_sizes)))
+
+    # Prescription
+    best_step = max(b, galaxy_size)/20.
+
+    for ss in [0]:
+
+        # Loop on step size
+        for ii, step_size in enumerate(step_sizes):
+            print(f'Step size: {step_size}')
+
+            # Calculate
+            L_wx, p_wOi, grid_p, p_xOis = bayesian.px_Oi_fixedgrid(
+                box_hwidth, path.localiz, np.array([gal_coord]),
+                np.array([galaxy_size]), theta_prior, step_size=step_size, 
+                return_debug=True)
+
+            # Save
+            raw_pxO[ss, ii] = p_xOis
+
+            # Correction
+            if b > galaxy_size:
+                cor_pxO[ss, ii] = p_xOis / np.sum(p_wOi) / step_size**2
+            else:
+                cor_pxO[ss, ii] = p_xOis / np.sum(L_wx) / step_size**2
+            #assert (np.sum(L_wx) * step_size**2) > 0.98, f'L_wx: {np.sum(L_wx) * step_size**2}'
+
+    # Normalize by the first step size
+    for ss in [0]:
+        cor_pxO[ss, :] /= cor_pxO[ss, 0]
+        raw_pxO[ss, :] /= raw_pxO[ss, 0]
+
+    title = f'Localization, a={a}", b={b}; offset by {gal_offset}"; PA={gal_pa}"'
+    plot_results(outfile, raw_pxO, cor_pxO, 
+                 np.array([galaxy_size]), step_sizes, best_step,
+                 var_name='Galaxy size', title=title)
 
 def big_galaxy(galaxy_size:float=20.):
 
@@ -246,4 +300,11 @@ if __name__ == '__main__':
 
     #big_localization()
     #big_galaxy()
-    gbo_localization()
+    #gbo_localization()
+
+    # ############
+    # Individual cases
+    #single_case('single_case.png', 
+    #            5., 5., 2., gal_offset=7.)
+    single_case('single_case.png', 
+                20., 0.2, 2., gal_offset=2., gal_pa=90.)
