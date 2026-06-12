5b0de123bc568d78b917d8c975a537eb859b4cf7
J. Xavier Prochaska <xavier@ucolick.org>
2026-06-12 06:51:51 -0700

    step_size

--- astropath/run.py
+++ astropath/run.py
@@ -184,31 +184,29 @@ def run_on_dict(idict: dict,
     p_O = Path.calc_priors()
 
     # Calculate step size based on localization and galaxy sizes
+    step_size = None
     if idict['ltype'] == 'eellipse':
-        # OLD
-        #a = eellipse['a']
-        #b = eellipse['b']
-        #step_size_max = 2 * 3 * np.nanmin([a, b]) / np.nanmax(cut_catalog['ang_size'])
-        #step_size = np.nanmin([0.1, step_size_max])
-        # New
-        min_ang = np.nanmin(cut_catalog['ang_size'].data)
-        if eellipse['b'] > min_ang:
-            correction = 'p_wO'
-            step_size = eellipse['b'] / 20.
-        else:
-            correction = 'L_wx'
-            step_size = min_ang / 20.
+        if idict['pmode'] == 'fixed':
+            min_ang = np.nanmin(cut_catalog['ang_size'].data)
+            if eellipse['b'] > min_ang:
+                correction = 'p_wO'
+                step_size = eellipse['b'] / 20.
+            else:
+                correction = 'L_wx'
+                step_size = min_ang / 20.
+        elif idict['pmode'] == 'local':
+            step_size = 0.05
+            correction = None
     else:
         # For healpix, use default step size
         raise ValueError("Healpix localization is not supported yet.")
-        step_size = 0.1
 
     # Add to dict
     idict['step_size'] = step_size
 
     # Calculate posteriors
     if idict['pmode'] == 'local':
-        print(f'Calculating posteriors with local') 
+        print(f'Calculating posteriors with local and step_size: {step_size}') 
         P_Ox, P_Ux = Path.calc_posteriors('local',
                                        box_hwidth=box_hwidth,
                                        survey_radius=idict['ssize']*60,
